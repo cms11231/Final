be5bcc13802f008107ea0124b658faf9b479bec5
Chelsea Smith <cms11231@gmail.com>
2020-05-24 20:54:13 -0600

    fix topbar and change formatting

--- swim.js
+++ swim.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", ready);
 
+function openProgress(evt, tabName) {
+      var i, tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+      document.getElementById(tabName).style.display = "block";
+      evt.currentTarget.className += " active";
+}
 
 function newSet() {
     let elem = document.createElement('p');
@@ -77,16 +90,16 @@ function newSet() {
 
 window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
 
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
 
 function newtime() {
     let time = document.getElementById('time');
